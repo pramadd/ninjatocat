5494cb6134be04bb55fdf890c8689d0047a91cb2
Pranathi <neethu@codingdojo.com>
2017-09-29 13:54:01 -0700

    ninjatocat

---
+++ ninjacat.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+
+$('img').click(function() {
+     var a = $(this).attr("src");
+     var b = $(this).attr("ninjasrc");
+     
+     $(this).attr("src",b);
+     $(this).attr("alt",a);
+    
+});
+
+});
